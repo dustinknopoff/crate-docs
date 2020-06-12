2ed17734d4f4751c6c1b83d26f863c5ab2caf979
Dustin Knopoff <dustinknopoff@gmail.com>
2020-06-12 11:05:22 -0400

    ci(fix): use relative installed dashing for building docsets

--- build-docsets.sh
+++ build-docsets.sh
@@ -4,5 +4,5 @@ while read line;
 do
 echo "Building $line docset"
 rsdocs-dashing target/doc/$line $line
-$GOPATH/bin/dashing build --config $line/dashing.json --source $line/build
+./dashing/bin/dashing build --config $line/dashing.json --source $line/build
 done < crates.txt
