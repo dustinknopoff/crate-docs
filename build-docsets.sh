a7564b50dac511433957cdf4af082d4a2361d7ea
Dustin Knopoff <dustinknopoff@gmail.com>
2020-06-12 18:12:09 -0400

    ci(include xmls): for private dash feeds

--- build-docsets.sh
+++ build-docsets.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-while read line;
+while read i;
 do
+line=`echo $i | awk -F ',' '{print $1}'`
+version=`echo $i | awk -F ',' '{print $2}'`
 echo "Building $line docset"
 rsdocs-dashing target/doc/$line $line
 ./dashing/bin/dashing build --config $line/dashing.json --source $line/build
+echo " \
+<entry> \
+<version>0.10.26</version> \
+<url>https://github.com/dustinknopoff/crate-docs/releases/download/latest/$line.tgz</url> \
+</entry> \
+" > $line.xml
+
 done < crates.txt
